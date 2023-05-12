a7be23921bb12d6a94d6168e820fa165aee9ddf3
mj06879 <mj06879@st.habib.edu.pk>
2023-05-12 13:45:52 +0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -19,15 +19,15 @@ class AnalysisPlot():
         
         # decay, we'll see
         
-    def QL_Reward_Ep(self,epsilon = 0.5,learning_rate = 0.9 ,discount_rate = 0.8,decay_rate=0,no_of_demo=10):
-        no_of_episodes = [x for x in range(10,1000,100)]
+    def QL_Reward_Ep(self,epsilon = 0.5,learning_rate = 0.9 ,discount_rate = 0.9,decay_rate=0,no_of_demo=10):
+        no_of_episodes = [x for x in range(10,5000,100)]
         experiments = []
         
-        for i in self.epsilon:
-            rewards = []
-            for ep in no_of_episodes:
-                rewards.append(QLearning(i,learning_rate,discount_rate,decay_rate,ep,no_of_demo)[0])
-            experiments.append(copy.deepcopy(rewards)) 
+        # for i in self.epsilon:
+        #     rewards = []
+        #     for ep in no_of_episodes:
+        #         rewards.append(QLearning(i,learning_rate,discount_rate,decay_rate,ep,no_of_demo)[0])
+        #     experiments.append(copy.deepcopy(rewards)) 
         
         for i in self.learning_rate:
             rewards = []
@@ -35,36 +35,41 @@ class AnalysisPlot():
                 rewards.append(QLearning(epsilon,i,discount_rate,decay_rate,ep,no_of_demo)[0])
             experiments.append(copy.deepcopy(rewards))  
             
-        for i in self.discount_rate:
-            rewards = []
-            for ep in no_of_episodes:
-                rewards.append(QLearning(epsilon,learning_rate,i,decay_rate,ep,no_of_demo)[0])
-            experiments.append(copy.deepcopy(rewards))        
+        # for i in self.discount_rate:
+        #     rewards = []
+        #     for ep in no_of_episodes:
+        #         rewards.append(QLearning(epsilon,learning_rate,i,decay_rate,ep,no_of_demo)[0])
+        #     experiments.append(copy.deepcopy(rewards))        
         
         
         plt.title("Q-Learning Analysis Plot")   
         plt.ylabel("Average Reward")
         plt.xlabel("Number of Training Episodes")
+
+        plt.plot(no_of_episodes,experiments[0],label = "$\\alpha = 0.1$")
+        plt.plot(no_of_episodes,experiments[1],label = "$\\alpha = 0.5$")
+        plt.plot(no_of_episodes,experiments[2],label = "$\\alpha = 0.9$")
         
-        plt.plot(no_of_episodes,experiments[0],label = "$\epsilon = 0.1 $",linestyle = "--",color='red')
-        plt.plot(no_of_episodes,experiments[1],label = "$\epsilon = 0.5 $",linestyle = "-.",color='red')
-        plt.plot(no_of_episodes,experiments[2],label = "$\epsilon = 0.9 $",linestyle = "dotted",color='red')
+        # plt.plot(no_of_episodes,experiments[0],label = "$\epsilon = 0.1 $",linestyle = "--",color='red')
+        # plt.plot(no_of_episodes,experiments[1],label = "$\epsilon = 0.5 $",linestyle = "-.",color='green')
+        # plt.plot(no_of_episodes,experiments[2],label = "$\epsilon = 0.9 $",linestyle = "dotted",color='blue')
         
-        plt.plot(no_of_episodes,experiments[3],label = "$\\alpha = 0.1 $",linestyle = "--",color='green')
-        plt.plot(no_of_episodes,experiments[4],label = "$\\alpha = 0.5 $",linestyle = "-.",color='green')
-        plt.plot(no_of_episodes,experiments[5],label = "$\\alpha = 0.9 $",linestyle = "dotted",color='green')
-        
-        plt.plot(no_of_episodes,experiments[6],label = "$\gamma = 0.1 $",linestyle = "--",color='blue')
-        plt.plot(no_of_episodes,experiments[7],label = "$\gamma = 0.5 $",linestyle = "-.",color='blue')
-        plt.plot(no_of_episodes,experiments[8],label = "$\gamma = 0.9 $",linestyle = "dotted",color='blue')
+        # plt.plot(no_of_episodes,experiments[0],label = "$\\alpha = 0.1 $",linestyle = "--",color='red')
+        # plt.plot(no_of_episodes,experiments[1],label = "$\\alpha = 0.5 $",linestyle = "-.",color='green')
+        # plt.plot(no_of_episodes,experiments[2],label = "$\\alpha = 0.9 $",linestyle = "dotted",color='blue')
+    
+        # plt.plot(no_of_episodes,experiments[6],label = "$\gamma = 0.1 $",linestyle = "--",color='blue')
+        # plt.plot(no_of_episodes,experiments[7],label = "$\gamma = 0.5 $",linestyle = "-.",color='blue')
+        # plt.plot(no_of_episodes,experiments[8],label = "$\gamma = 0.9 $",linestyle = "dotted",color='blue')
         
         plt.legend()
         plt.show()
 
-    def SarsaLearning_Reward_Ep(self,learning_rate,discount_rate,no_of_demo=50):
+    def SarsaLearning_Reward_Ep(self,learning_rate=0.9,discount_rate=0.9,no_of_demo=10):
         no_of_episodes = [x for x in range(10,5000,100)]
         experiments = []
-        for i in [0.1,0.5,0.9]:
+        # for i in [0.1,0.5,0.9]:
+        for i in self.learning_rate:
             rewards = []
             for ep in no_of_episodes:
                 rewards.append(SarsaLearning(i,discount_rate,ep,no_of_demo)[0])
@@ -73,10 +78,10 @@ class AnalysisPlot():
         plt.title("SARSA-Learning Analysis Plot")   
         plt.ylabel("Average Reward")
         plt.xlabel("Number of Training Episodes")
-        
-        plt.plot(no_of_episodes,experiments[0],label = "$\\alpha = 0.1, \gamma = 0.9$")
-        plt.plot(no_of_episodes,experiments[1],label = "$\\alpha = 0.5, \gamma = 0.9$")
-        plt.plot(no_of_episodes,experiments[2],label = "$\\alpha = 0.9, \gamma = 0.9$")
+
+        plt.plot(no_of_episodes,experiments[0],label = "$\\alpha = 0.1$")
+        plt.plot(no_of_episodes,experiments[1],label = "$\\alpha = 0.5$")
+        plt.plot(no_of_episodes,experiments[2],label = "$\\alpha = 0.9$")
         plt.legend()
         plt.show()
 
@@ -111,6 +116,7 @@ class AnalysisPlot():
 #####################
 RLproject = AnalysisPlot()
 RLproject.QL_Reward_Ep()
+# RLproject.SarsaLearning_Reward_Ep()
 # RLproject.Compartive_analysis_reward(epsilon=0.5,learning_rate=0.8,discount_rate=0.9,decay_rate=0)    
         
 
